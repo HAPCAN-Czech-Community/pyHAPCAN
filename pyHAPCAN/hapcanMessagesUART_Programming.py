d6b1e2a36d412040eafd6ce4e13cc6012e590421
Ondrej Kolar <xkolar74@vutbr.cz>
2025-11-23 15:43:56 +0100

    Implement address and data programming messages

--- pyHAPCAN/hapcanMessagesUART_Programming.py
+++ pyHAPCAN/hapcanMessagesUART_Programming.py
@@ -19,5 +19,73 @@ class EXIT_ONE_BOOTLOADER(HapcanMessageUART):
         return data
 
 
-#TBD    ADDRESS_FRAME = 0x030
-#TBD    DATA_FRAME = 0x040
+class ADDRESS_FRAME(HapcanMessageUART):
+    # 0xAA 0x030 0x0 ADRU ADRH ADRL 0xXX 0xXX CMD 0xXX 0xXX CHKSUM 0xA5
+    FRAME_TYPE = 0x0300
+
+    def __init__(self, addr, cmd, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.addr = addr
+        self.cmd = cmd
+
+    @classmethod
+    def from_bytes(cls, data: bytearray):
+        addr = (data[3] << 16) | (data[4] << 8) | data[5]
+        cmd = data[8]
+        msg = cls(addr=addr, cmd=cmd)
+        return msg
+    
+    def to_bytes(self):
+        addrU = (self.addr >> 16) & 0xFF
+        addrH = (self.addr >> 8) & 0xFF
+        addrL = self.addr & 0xFF
+        data = bytearray([addrU, addrH, addrL, 0xFF, 0xFF, self.cmd, 0xFF, 0xFF])
+        self._prepend_type(data, self.FRAME_TYPE)
+        self._append_checksum(data)
+        self._append_header_trailer(data)
+        return data
+    
+    def makeResponse(self):
+        return ADDRESS_FRAME_RESP(addr=self.addr, cmd=self.cmd)
+    
+
+class ADDRESS_FRAME_RESP(ADDRESS_FRAME):
+    # 0xAA 0x030 0x1 echo echo echo echo echo echo echo echo CHKSUM 0xA5
+    FRAME_TYPE = 0x0301
+    # Methods are inherited from ADDRESS_FRAME
+
+
+class DATA_FRAME(HapcanMessageUART):
+    # 0xAA 0x040 0x1 DATA0 DATA1 DATA2 DATA3 DATA4 DATA5 DATA6 DATA7 CHKSUM 0xA5
+    FRAME_TYPE = 0x0400
+
+    def __init__(self, dataBytes: bytearray, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if len(dataBytes) != 8:
+            raise ValueError("dataBytes must be exactly 8 bytes long")
+        self.dataBytes = dataBytes
+
+    @classmethod
+    def from_bytes(cls, data: bytearray):
+        dataBytes = data[3:11]
+        msg = cls(dataBytes=dataBytes)
+        return msg
+    
+    def to_bytes(self):
+        data = bytearray(self.dataBytes)
+        self._prepend_type(data, self.FRAME_TYPE)
+        self._append_checksum(data)
+        self._append_header_trailer(data)
+        return data
+    
+    def makeResponse(self):
+        return DATA_FRAME_RESP(dataBytes=self.dataBytes)
+    
+
+class DATA_FRAME_RESP(DATA_FRAME):
+    # 0xAA 0x040 0x1 echo echo echo echo echo echo echo echo CHKSUM 0xA5
+    FRAME_TYPE = 0x0401
+    # Methods are inherited from DATA_FRAME
+
+
+#TBD    ERROR_FRAME = 0x0F0
--- pyHAPCAN/hapcanMessages_Programming.py
+++ pyHAPCAN/hapcanMessages_Programming.py
@@ -47,5 +47,81 @@ class EXIT_ONE_BOOTLOADER(HapcanMessage):
         return (self.targetGroup == device.groupId) and (self.targetNode == device.nodeId)
     
 
-#TBD    ADDRESS_FRAME = 0x030
-#TBD    DATA_FRAME = 0x040
+class ADDRESS_FRAME(HapcanMessage):
+    # 0xAA 0x030 0x0 MODULE GROUP ADRU ADRH ADRL 0xXX 0xXX CMD 0xXX 0xXX CHKSUM 0xA5
+    FRAME_TYPE = 0x0300
+
+    def __init__(self, targetNode, targetGroup, addr, cmd, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.targetNode = targetNode
+        self.targetGroup = targetGroup
+        self.addr = addr
+        self.cmd = cmd
+
+    @classmethod
+    def from_bytes(cls, data: bytearray):
+        targetNode = data[3]
+        targetGroup = data[4]
+        addr = (data[5] << 16) | (data[6] << 8) | data[7]
+        cmd = data[10]
+        msg = cls(targetNode=targetNode, targetGroup=targetGroup, addr=addr, cmd=cmd)
+        return msg
+
+    def to_bytes(self):
+        addrU = (self.addr >> 16) & 0xFF
+        addrH = (self.addr >> 8) & 0xFF
+        addrL = self.addr & 0xFF
+        data = bytearray([self.targetNode, self.targetGroup, addrU, addrH, addrL, 0xFF, 0xFF, self.cmd, 0xFF, 0xFF, 0xFF])
+        self._prepend_type(data, self.FRAME_TYPE)
+        self._append_checksum(data)
+        self._append_header_trailer(data)
+        return data
+    
+    def makeResponse(self):
+        return ADDRESS_FRAME_RESP(addr=self.addr, cmd=self.cmd)
+    
+
+class ADDRESS_FRAME_RESP(ADDRESS_FRAME):
+    # 0xAA 0x030 0x1 MODULE GROUP echo echo echo echo echo echo echo echo CHKSUM 0xA5
+    FRAME_TYPE = 0x0301
+    # Methods are inherited from ADDRESS_FRAME
+
+
+class DATA_FRAME(HapcanMessage):
+    # 0xAA 0x040 0x1 MODULE GROUP DATA0 DATA1 DATA2 DATA3 DATA4 DATA5 DATA6 DATA7 CHKSUM 0xA5
+    FRAME_TYPE = 0x0400
+
+    def __init__(self, targetNode, targetGroup, dataBytes, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if len(dataBytes) != 8:
+            raise ValueError("dataBytes must be exactly 8 bytes long")
+        self.targetNode = targetNode
+        self.targetGroup = targetGroup
+        self.dataBytes = dataBytes
+
+    @classmethod
+    def from_bytes(cls, data: bytearray):
+        targetNode = data[3]
+        targetGroup = data[4]
+        dataBytes = data[5:13]
+        msg = cls(targetNode=targetNode, targetGroup=targetGroup, dataBytes=dataBytes)
+        return msg
+    
+    def to_bytes(self):
+        data = bytearray([self.targetNode, self.targetGroup]) + bytearray(self.dataBytes)
+        self._prepend_type(data, self.FRAME_TYPE)
+        self._append_checksum(data)
+        self._append_header_trailer(data)
+        return data
+    
+    def makeResponse(self):
+        return DATA_FRAME_RESP(targetNode=self.targetNode, targetGroup=self.targetGroup, dataBytes=self.dataBytes)
+    
+
+class DATA_FRAME_RESP(DATA_FRAME):
+    # 0xAA 0x040 0x1 MODULE GROUP DATA0 DATA1 DATA2 DATA3 DATA4 DATA5 DATA6 DATA7 CHKSUM 0xA5
+    FRAME_TYPE = 0x0401
+    # Methods are inherited from DATA_FRAME
+
+
+#TBD    ERROR_FRAME = 0x0F0
